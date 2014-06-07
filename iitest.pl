395d02099a54b010849e64d2978d519034322c53
Vasiliy Vylegzhanin <v.gadfly@gmail.com>
2014-06-07 14:34:17 +0400

    cyr test

--- iitest.pl
+++ iitest.pl
@@ -5,11 +5,14 @@ use II::Point;
 
 use Data::Dumper;
 
-my $ii = II::Point->new ("http://51t.ru/");
-my %echoes = $ii->fetch_echoes('im.1406');
+binmode STDOUT, ':utf8';
+
+my $ii = II::Point->new ("http://irk38.tk/ii/ii-point.php?q=/");
+my %echoes = $ii->fetch_echoes('im.100');
 print Dumper (\%echoes);
 
 for my $echo (keys %echoes) {
     my %msg = $ii->fetch_msgs(@{$echoes{$echo}}[0..9]);
-    print Dumper ($echo, \%msg);
+    print $msg{$_}{content}, "\n===="
+    	for (keys %msg);
 }
